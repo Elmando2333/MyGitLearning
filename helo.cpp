ec3770d7edf9029ec7d8f0d3294ad7c3b03374be
Elmando <1091992414@qq.com>
2021-01-06 17:25:39 +0800

    Just add a for loop

--- helo.cpp
+++ helo.cpp
@@ -6,5 +6,9 @@ int main()
     cout << "Hello world!" << endl;
     cout << 1 << endl;
     cout << 2 << endl;
+    for(int i=0;i<10;i++)
+    {
+        cout<<"hihihihi"<<endl;
+    }
     return 0;
 }
